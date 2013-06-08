210b86d153f1dc3de2623eec82e6f26d889a1a70
Dick Barends <dick.barends@wxs.nl>
2013-06-08 23:25:44 +0200

    Eerste Plot functie van H.W.M. Barends

--- dckFunctions.m
+++ dckFunctions.m
@@ -1,4 +1,13 @@
-
+dckPlotBasic[x_,y_List,z_List,legenda_List] :=
+    Plot[x,y,
+        GridLines->Automatic,
+        GridLinesStyle->Directive[LightGray,Dashed],
+        PlotLabel->z[[1]],
+        AxesLabel->{z[[2]],z[[3]]},
+        ImageSize->500,
+        AspectRatio->0.5,
+        PlotLegends->legenda
+    ]
 
 (* Mathematica Raw Program *)
 
