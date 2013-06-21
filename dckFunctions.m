ac7bbedd5bca2bc861edd45db7e4a355b934c4b4
Dick Barends <dick.barends@wxs.nl>
2013-06-21 14:31:28 +0200

    Added some graphical functions

--- dckFunctions.m
+++ dckFunctions.m
@@ -1,8 +1,14 @@
+(* 
+This file contains functions to easily read, present en analyse data in an excel
+file. The excel file needs to have a particular layout.
+*)
+
+
 dckPlotBasic[f_,            (* f: function to Plot                            *) 
-	     x_List,        (* x: List with variable and start and end value  *)
-	     z_List,        (* z: List with Plolabel en AxesLabel             *)
-	     legenda_List   (* legenda: List met legenda strings              *)
-	     ] :=
+         x_List,        	(* x: List with variable and start and end value  *)
+         z_List,        	(* z: List with Plolabel en AxesLabel             *)
+         legenda_List   	(* legenda: List met legenda strings              *)
+         ] :=
     Plot[f,x,
         GridLines->Automatic,
         GridLinesStyle->Directive[LightGray,Dashed],
@@ -15,9 +21,30 @@ dckPlotBasic[f_,            (* f: function to Plot                            *)
 
 
 dckReadPIData[file_String    (* Name of the data file *) ] :=
-Module[{data},
-	data = Import[file];
-	{ data[[2]], 
-		Transpose[data[[1]][[{1,2}]]],
-		data[[1]][[Range[13,Length[data[[1]] ] ] ]] }
-]
+    Module[ {data},
+        data = Import[file];
+        { data[[2]], 
+            Transpose[data[[1]][[{1,2,3}]]],
+            data[[1]][[Range[13,Length[data[[1]] ] ] ]] }
+    ]
+
+dckDateListPlot[data_,n_] :=
+    Module[ {},
+        DateListPlot[Transpose[Transpose[data[[3]]][[{1, n}]]],
+            PlotLabel->data[[2]][[n]][[2]]<>"\nfrom "<>DateString[data[[1]][[1, 2]]]<>
+                " to "<>DateString[data[[1]][[2, 2]]],
+            AxesLabel->{"Tijd",data[[2]][[n]][[3]]},
+            ImageSize->500,
+            AspectRatio->0.5
+        ]
+    ]
+	
+	
+dckDynamicDatelistPlot[data_] :=
+    DynamicModule[ {},
+    	Manipulate[
+    		dckDateListPlot[data,Position[data[[2]][[All, 2]], description][[1, 1]]],
+    		{description, Drop[data[[2]][[All, 2]], 1]},
+    		ControlType -> PopupMenu
+    	]
+    ]
